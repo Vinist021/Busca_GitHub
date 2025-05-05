09fa3d980e0607eb87e5096e459236488f580f5e
Vinicius Teixeira <vinist.002@gmail.com>
2025-05-04 22:09:10 -0300

    Closes #2 - Pesquisa de usuário concluída

---
+++ script.js
@@ -0,0 +1,17 @@
+
+function search() {
+    var username = document.getElementById('inputUsername').value;
+
+    var url = `https://api.github.com/users/${username}`;
+
+    $.getJSON(url, (user) => {
+        
+        document.getElementById("name").innerHTML = user.name;
+        document.getElementById("html_url").innerHTML = user.html_url;
+        document.getElementById("company").innerHTML = user.company;
+        document.getElementById("avatar_url").innerHTML = `
+         <img src="${user.avatar_url}" alt="avatar" width="220" height="220" class="shadow rounded">
+        `;
+
+    });
+}
